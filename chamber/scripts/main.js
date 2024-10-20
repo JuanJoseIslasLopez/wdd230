369e579c5090c6a6e3a29cd61fd678ae0e371ab6
Juan José Islas Lopez <jujojo97@gmail.com>
2024-10-20 01:50:47 -0300

    chamber fixed

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -3,7 +3,14 @@ document.getElementById('last-updated').textContent = document.lastModified;
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
 
-hamButton.addEventListener('click', () => {
-    navigation.classList.toggle('open');
-    hamButton.classList.toggle('open'); 
+document.addEventListener("DOMContentLoaded", function () {
+    const menuButton = document.getElementById('menu');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuButton.addEventListener('click', function () {
+        // Toggle the 'open' class for both button and navigation
+        menuButton.classList.toggle('open');
+        navLinks.classList.toggle('open');
+    });
 });
+
